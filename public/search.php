7426f4d0768d5c601f6cdb3058126d8e072eee6e
Charisse <charisse.ysabel930@gmail.com>
2015-11-09 00:55:12 +0100

    Fix

--- public/search.php
+++ public/search.php
@@ -5,7 +5,15 @@
     // numerically indexed array of places
     $places = [];
 
-    // TODO: search database for places matching $_GET["geo"]
+    $geoVar = urldecode($_GET["geo"]);
+
+    // search database for places matching $_GET["geo"]
+    $search = query("SELECT * FROM places WHERE
+        MATCH(postal_code, place_name, admin_name1, admin_code1)
+        AGAINST(? IN BOOLEAN MODE)", $_GET["geo"] );
+    
+    
+    $places = $search;
 
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
